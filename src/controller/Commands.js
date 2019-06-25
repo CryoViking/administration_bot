f6c2329e5fcf76cc43b18823f42792e0fe434aaf
Xiao <xiaonai.he43@gmail.com>
2019-06-25 00:17:42 -0400

    Cryosis made change thx to xiao

--- src/controller/Commands.js
+++ src/controller/Commands.js
@@ -1,9 +1,19 @@
+const downloader = require('./Downloader.js');
+const snippet = require('../view/ViewConstants.js');
+
 module.exports = {
     commandSwitch: async function commandSwitch(msg){
         var args = msg.content.substring(1).split(" ");
         switch(args[0]){
+            /* Command description: 
+             */
             case "ping":
                 await ping(msg);
+                break;
+            /* Command description:
+             * Saves the first attachment of the message to disk and displays it's contents, if able.
+             */
+            case "import":
                 break;
             default:
                 msg.channel.send("This is not a valid command.");
@@ -13,4 +23,32 @@ module.exports = {
 
 async function ping(msg){
     msg.channel.send("Pong!");
+}
+
+async function readAttachment(msg){
+    var author = msg.author.id;
+    let url = msg.attachments.first().url;
+    msg.channel.send("Reading the file! Fetching Data...");
+    try{
+        var body = await downloader.download(author, url);
+        let maxChar = 2000;
+        let snipperChars = 8;
+        if(body.length > maxChar - snippetChar){
+            msg.channel.send("File contents exceed char count. Cannot paste content.");
+        }
+        else{
+            msg.channel.send({embed: {
+                author:{
+                    name: msg.author.username,
+                    icon_url: msg.author.avatarURL
+                },
+                title: "File Contents",
+                content: `${snippet.TRIPLE_BACKTICK}${body}${snippet.TRIPLE_BACKTICK}`
+            }});
+        }
+    }
+    catch(err){
+        msg.channel.send("There was an error in reading the file.");
+        console.log(err.toString());
+    }
 }
---
+++ src/controller/Downloader.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+module.exports = {
+    /**
+     * This function will return the body of the requested url.
+     */
+    request: async function(url) {
+        return new Promise(function(resolve, reject){
+            request(url, (err, resp, body)=>{
+                if(err) reject();
+                resolve(body)
+            });
+        })
+    }, //is this suppose to b a , or ;
+    /**
+     * This funciton will request the body of the passed url, and download it to disk.
+     * And return the body of the request. 
+     */
+    download: async function(author, url){
+        var body = await this.request(url);
+        //If one author uploads to files with same name. Override first with second.
+        var filename = `${author}_${this.extractFileName(url)}`;
+        this.saveFile(filename, body);
+        return body;
+    }
+}
+
+function extractFileName(url){
+    var args = url.split("/");
+    return args[args.length - 1];
+}
+
+function saveFile(filename, contents){
+    var destination = `./cache/temp/file/${filename}`;
+    fs.writeFile(destination, contents, (err) => {
+        if(err) console.log(err);
+        console.log(`Saved file to disk\ndesntion:${contents}`);
+    })
+}
