6ae9055f355cde97afca92266a531dc8fee5838c
PotatoMaster101 <PotatoMaster101@users.noreply.github.com>
2019-06-26 10:36:55 -0700

    Add PyExec

---
+++ src/controller/PyExec.js
@@ -0,0 +1,15 @@
+// Executes python scripts. 
+
+const {spawn} = require('child_process');
+
+module.exports = {
+    run: async function(name, args) {
+        
+    }
+}
+
+// Returns the script path. (/py/src/name)
+function getScriptPath(name) {
+    
+}
+
