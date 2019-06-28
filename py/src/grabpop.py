bed25df915ec8df7b9eaff5d255ca01f1c48ecfa
PotatoMaster101 <PotatoMaster101@users.noreply.github.com>
2019-06-28 07:05:09 -0700

    Progress on population grabber

--- py/src/grabpop.py
+++ py/src/grabpop.py
@@ -8,16 +8,35 @@ import requests
 import json
 import time
 
+# use .format() on me
+APIBASE = "https://discordapp.com/api/{}"
+
 def get_args():
     """
     Get user arguments. 
     """
     p = argparse.ArgumentParser(description="Bot population grabber.")
-    p.add_argument("conf", type=str, dest="conf", 
+    p.add_argument("conf", type=str, 
             help="the BOT config file path (json file)")
     p.add_argument("-i", "--interval", type=int, dest="intv", default=3600, 
             help="the interval in seconds, defaults to 3600")
     return p
+
+
+def get_json(raw):
+    """
+    Returns the JSON from raw. 
+    """
+    return json.loads(raw)
+
+
+def get_conf_json(conf):
+    """
+    Returns the config file as JSON. 
+    """
+    with open(conf) as f:
+        return json.loads(f.read())
+
 
 if __name__ == "__main__":
     """
@@ -25,9 +44,8 @@ if __name__ == "__main__":
     """
     args = get_args().parse_args()
     interval = args.intv       # interval in seconds, default 3600
-
-    while True:
-        
-        time.sleep(interval)
-    print(interval)
+    # TODO make checks on interval (60 sec min)
+    conf = get_conf_json(args.conf)
+    botid = conf["id"]
+    guid = conf["guild_id"]
 
